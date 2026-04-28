977267f7e26b39ac36ec3cbd290b9333e5ef67e0
Student Spare <student@Mac.lan>
2026-04-27 21:41:01 -0400

    fix: move setVisible(true) back to the last line, now everthing in stage 1 works

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ public class Main {
         JFrame frame = new JFrame("Qbert Pixel Canvas");
         frame.setSize(600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         CanvasPanel canvas = new CanvasPanel();
         frame.add(canvas);
+        frame.setVisible(true);
 
     }
 }
